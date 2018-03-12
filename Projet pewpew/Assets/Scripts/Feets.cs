f742bf51fcae03c4b3cccf9b137753555b4bce77
Kazzad <lekazzad@gmail.com>
2018-03-12 23:08:28 +0100

    Ajout de code
    Character Control

---
+++ Projet pewpew/Assets/Scripts/Feets.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feets : MonoBehaviour {
+
+	PlayerController player;
+
+	void Start () {
+		player = transform.parent.GetComponent<PlayerController> ();
+	}
+
+	void OnTriggerEnter (Collider coll) {
+		player.isTouchingTheGround = true;
+	}
+
+	void OnTriggerExit (Collider coll) {
+		player.isTouchingTheGround = false;
+	}
+}
---
+++ Projet pewpew/Assets/Scripts/KeysConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeysConfig : MonoBehaviour {
+	public KeyCode forward, back, left, right, jump; // Touches directionnelles.
+	public KeyCode sprint, crouch; // Modes de déplacement.
+
+	public float mouseSensitivity; // Sensibilité de la souris.
+
+	void Start () {
+		// Assignation pour les tests.
+		forward = KeyCode.Z;
+		back = KeyCode.S;
+		left = KeyCode.Q;
+		right = KeyCode.D;
+		jump = KeyCode.Space;
+
+		sprint = KeyCode.R;
+		crouch = KeyCode.T;
+
+		mouseSensitivity = 10;
+	}
+}
