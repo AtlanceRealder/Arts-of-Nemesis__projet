c4b955422b794246342447d211ea3018f8076089
Kazzad <lekazzad@gmail.com>
2018-03-27 21:24:23 +0200

    Pathfinding IA

---
+++ Projet pewpew/Assets/Scripts/AI/NodeGrid.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeGrid : MonoBehaviour {
+	// Contient tous les nodes.
+	private Node[,] grid;
+	// Les zones ou l'IA ne peut pas marcher.
+	public LayerMask unwalkableMask;
+
+	// La taille de la grille.
+	public Vector2 gridWorldSize;
+	// La taille de la grille en nodes.
+	private int gridSizeX, gridSizeY;
+
+	// Le rayon et le diamètre d'un node.
+	public float nodeRadius;
+	private float nodeDiameter;
+
+	public bool drawNodes;
+
+	// A partir d'un point, calcule le node correspondant.
+	public Node NodeFromWorldPoint (Vector3 worldPosition) {
+		float percentX = Mathf.Clamp01 ((worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x), percentY = Mathf.Clamp01 ((worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y);
+		int x = Mathf.RoundToInt ((gridSizeX - 1) * percentX), y = Mathf.RoundToInt ((gridSizeY - 1) * percentY);
+
+		return grid [x, y];
+	}
+
+
+	public List<Node> GetNeighbours (Node node) {
+		List<Node> neighbours = new List<Node> ();
+
+		for (int x = -1; x <= 1; x++) {
+			for (int y = -1; y <= 1; y++) {
+				if (x == 0 && y == 0)
+					continue;
+
+				int checkX = node.gridX + x, checkY = node.gridY + y;
+
+				if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+					neighbours.Add (grid [checkX, checkY]);
+			}
+		}
+
+		return neighbours;
+	}
+
+	void Awake () {
+		nodeDiameter = nodeRadius * 2;
+
+		gridSizeX = Mathf.RoundToInt (gridWorldSize.x / nodeDiameter);
+		gridSizeY = Mathf.RoundToInt (gridWorldSize.y / nodeDiameter);
+
+		CreateGrid ();
+	}
+
+	// Créé la grille de node (pré-analyse de la carte).
+	void CreateGrid () {
+		Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+
+		grid = new Node[gridSizeX, gridSizeY];
+
+		for (int x = 0; x < gridSizeX; x++) {
+			for (int y = 0; y < gridSizeY; y++) {
+				// La position du node.
+				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+				// Un élément non praticable (unwalkable) sur le node ?
+				bool walkable = !(Physics.CheckSphere (worldPoint, nodeRadius, unwalkableMask));
+
+				grid [x, y] = new Node (walkable, worldPoint, x, y);
+			}
+		}
+	}
+		
+
+	public List<Node> path = new List<Node> ();
+	void OnDrawGizmos () {
+		// La taille de la grille (visualisable en éditeur).
+		Gizmos.DrawWireCube (transform.position, new Vector3 (gridWorldSize.x, 1, gridWorldSize.y));
+
+		// Les nodes: praticable en blanc, sinon en rouge (visualisable en playmode).
+		if (grid != null && drawNodes) {
+			foreach (Node n in grid) {
+				Gizmos.color = (n.walkable) ? Color.white : Color.red;
+
+				if (path != null && path.Contains (n)) {
+					Gizmos.color = Color.black;
+				}
+				
+				Gizmos.DrawCube (n.worldPosition, new Vector3 ((nodeDiameter - .1f), 0.2f, (nodeDiameter - .1f)));
+			}
+		}
+	}
+}
--- Projet pewpew/Assets/Scripts/Weapon.cs
+++ Projet pewpew/Assets/Scripts/Weapon.cs
@@ -4,31 +4,33 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Weapon : MonoBehaviour {
-	public Transform arm, head, aimer;
-	// fireDelay définit le temps requis entre chaque tirs; reloadTime le temps requis pour recharger. (Tous deux en secondes).
+	// 'fireDelay' définit le temps requis entre chaque tirs; 'reloadTime' le temps requis pour recharger. (Tous deux en secondes).
 	public float fireDelay, reloadTime;
-	// bodyDamages et headDamages définissent les dégats infligés lors de touches au corps ou à la tête.
-	// range définit la portée de l'arme, recoil le recul et dispersion l'angle maximal de dispersion sur un axe.
-	public int bodyDamages, headDamages, range, recoil, dispersion;
-	// magazinesRemaining définit le nombre de chargeurs restants. magazineCapacity définit le nombre de munitions dans un chargeur.
+	// 'bodyDamages' et 'headDamages' définissent les dégats infligés lors de touches au corps ou à la tête. 'range' définit la portée de l'arme. 'bulletsPerShot' le nombre de projectiles tirés par tir.
+	public int bodyDamages, headDamages, range, bulletsPerShot;
+	// 'recoil' définit le recul, 'dispersion' l'angle maximal de dispersion sur un axe, et 'bulletDrop' la retombée de la balle en fonction de la distance.
+	public float recoil, dispersion, bulletDrop;
+	// 'magazinesRemaining' définit le nombre de chargeurs restants. 'magazineCapacity' définit le nombre de munitions dans un chargeur.
 	public int magazinesRemaining, magazineCapacity;
 	// Définit si arme de melée ou arme à feu.
 	public bool meleeWeapon;
 
 	public AudioClip fireAudio, reloadAudio, bulletShellAudio;
 
+	private Transform head, aimer; // Les transforms des différentes parties du corps utiles au tir.
+
 	private bool canFire;
-	private int bulletsRemaining; // The amount of remaining bullets in the currently used magazine.
+	private int bulletsRemaining; // Le nombre de balles restantes dans le chargeur en cours d'utilisation.
+	private int currentRecoil = 1; // Le coefficient de recul actuel.
+
 	private AudioSource audioSource;
 
-	public GameObject prefabTest;
-
+	// HUD de tests.
 	private Text ammo, mag;
 
 	void Start () {
 		audioSource = GetComponent<AudioSource> ();
-		arm = transform.parent.parent;
-		head = arm.parent.parent;
+		head = transform.parent.parent.parent.parent;
 		aimer = transform.GetChild (0);
 
 		canFire = true;
@@ -77,27 +79,47 @@ public class Weapon : MonoBehaviour {
 			Camera cam = head.GetComponent<Camera> ();
 			Ray ray = cam.ScreenPointToRay (new Vector3 (cam.pixelWidth / 2, cam.pixelHeight / 2, 0));
 
-			Vector3 disp = new Vector3 (Random.Range (0, dispersion), Random.Range (0, dispersion), 0); // Système de dispersion a chier parce que fait en 5min a 3h du matin.
-			ray.direction += disp;
+			if (Physics.Raycast (ray, out hit, range)) { // Créer un Raycast pour récupérer le point visé par le joueur.
+				for (int i = 0; i < bulletsPerShot; i++) {
+					// En fonction du point récupéré, calcule la direction de ce point par rapport au bout du canon de l'arme: 'dir'.
+					Vector3 heading = hit.point - aimer.transform.position, dir = heading / heading.magnitude;
 
-			if (Physics.Raycast (ray, out hit, range)) {
-				Vector3 heading = hit.point - aimer.transform.position, dir = heading / heading.magnitude;
+					float currentBulletDrop = Vector3.Distance (aimer.position, hit.point) * bulletDrop / 100;
 
-				if (Physics.Raycast (aimer.position, dir, out hit2)) {
-					// Pan! Touché!
+					// Calcule la dispersion.
+					Vector3 disp = new Vector3 (Random.Range (0, dispersion) / 50, Random.Range (0, dispersion) / 50 - currentBulletDrop, Random.Range (0, dispersion) / 50);
+
+					// La direction du tir prenant en compte la dispersion.
+					Vector3 dispDir = dir + disp;
+
+
+				
+					if (Physics.Raycast (aimer.position, dispDir, out hit2)) {
+						// Quelque chose est touché.
+					}
+				
+					Debug.DrawLine (aimer.position, hit2.point, Color.red, 20f); // Debug le tir.
+					Debug.Log(hit2.point + "  " + disp + "  " + dir + "  " + dispDir);
 				}
-
-				Debug.DrawLine (aimer.position, hit2.point, Color.red, 20f);
 			}
+				
+			bulletsRemaining--; // Enlève la balle tirée du chargeur.
+			UpdateHUD (); // Met a jour le HUD.
 
-			bulletsRemaining--;
-			UpdateHUD ();
+			// Applique le l'effet de recul.
+			head.transform.Rotate (Random.Range (0, -1 * recoil * currentRecoil * 0.5f), 0, 0);
+			head.parent.Rotate (0, Random.Range (-1 * recoil * currentRecoil * 0.5f, 1 * recoil * currentRecoil * 0.5f), 0);
 
-			head.transform.Rotate (Random.Range (-recoil * 1.5f, -recoil), 0, 0);
-			head.parent.Rotate (0, Random.Range (-recoil * 1.5f, recoil * 1.5f), 0);
+			if (currentRecoil < 5) // Ajoute du recul.
+				currentRecoil++;
 
 			yield return new WaitForSeconds (fireDelay);
 			canFire = true;
+
+			yield return new WaitForSeconds (0.8f);
+
+			if (currentRecoil > 1) // Enlève du recul.
+				currentRecoil--;
 		}
 	}
 }
